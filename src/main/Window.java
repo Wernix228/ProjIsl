1ec4dcd193e541be82834a0de57ff0710f91249f
D-DoSHiRAk <islyam1013@gmail.com>
2022-11-04 19:08:14 +0400

    Update V(0.0.0.5)

---
+++ src/main/Window.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.*;
+
+public class Window {
+
+    public static JFrame frame = new JFrame();
+
+    public Window(int x, int y, int w, int h){
+
+        //frame.setBounds(x,y,w,h);
+
+    }
+
+}
