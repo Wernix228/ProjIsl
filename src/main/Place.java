0cd5d4b3629c0e337d26ca55be3484c8a1bd3659
D-DoSHiRAk <islyam1013@gmail.com>
2022-11-04 20:31:59 +0400

    Update V(0.0.0.12)

--- src/main/Place.java
+++ src/main/Place.java
@@ -1,13 +1,29 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Place extends JButton{
+public class Place extends JButton {
 
-    public Place(int x, int y){
-        setLocation(x,y);
-        setSize(200,200);
+    private static int turn=1;//0=нолик 1=крестик
+    public Place(int x, int y) {
+        setLocation(x, y);
+        setSize(200, 200);
         setVisible(true);
+        setFont(new Font("Arial", Font.PLAIN, 40));
+        addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(turn==1){
+                    setText("X");
+                    turn=0;
+                }else if(turn==0){
+                    setText("O");
+                    turn=1;
+                }
+            }
+        });
     }
 
 }
