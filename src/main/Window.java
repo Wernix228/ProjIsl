ff10e352aa7353c8b854478a29ed05ab480fb58e
Wernix <Ktoto2667@gmail.com>
2022-11-04 18:12:07 +0300

    update V(0.0.0.6)

--- src/main/Window.java
+++ src/main/Window.java
@@ -1,14 +1,18 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Window {
 
     public static JFrame frame = new JFrame();
 
-    public Window(int x, int y, int w, int h){
+    public Window(int x, int y) {
 
-        //frame.setBounds(x,y,w,h);
+        frame.setDefaultCloseOperation(3);
+        frame.setSize(new Dimension(x,y));
+//        frame.setLocation(null);
+        frame.setVisible(true);
 
     }
 
