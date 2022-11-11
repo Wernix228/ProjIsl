0e218ae63fb5c79a9731d5528ecc13393ee3abf6
Wernix228 <Ktoto2667@gmail.com>
2022-11-11 10:20:55 +0300

    update V(0.0.1.3)

--- Server/src/com/server/Server.java
+++ Server/src/com/server/Server.java
@@ -5,46 +5,46 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Server extends Thread {
-    private ServerSocket serverSocket;
+
+    private static ServerSocket serverSocket;
+    private Socket socket;
+    static int port;
     ArrayList<String> list = new ArrayList<>();
-    public static void main(String[] args) {
-        int port = 5050;
+
+    public static void main(String[] args) throws IOException {
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(10000);
+    }
+    public Server(int port) throws IOException {
+        socket = serverSocket.accept();
+    }
+
+    public void run() {
+        while (true) {
+
+        }
+    }
+
+    public synchronized void connection(String address) {
         try {
             Thread t = new Server(port);
             t.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public Server(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
-        serverSocket.setSoTimeout(10000);
-    }
-
-    public void run() {
-        while (true) {
-            if(list.size() < 2){
-                connection((serverSocket.getInetAddress().toString()));
-                try {
-                    Socket socket = serverSocket.accept();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-    public synchronized void connection(String address){
         list.add(address);
 
     }
-    public synchronized void sendMessage(){
+
+    public synchronized void sendMessage() {
 
     }
-    public synchronized void disconnect(){
+
+    public synchronized void disconnect() {
 
     }
-    public synchronized void onException(){
+
+    public synchronized void onException() {
 
     }
 }
